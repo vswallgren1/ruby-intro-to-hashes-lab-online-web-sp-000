165f2ea4d23d75aba1e4d2e2919a74a87feb7706
Vince W <vincewallgren@me.com>
2021-02-27 05:42:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,18 +1,18 @@
 def new_hash
-{}
+	{}
 end
 
 def actor
-{:name => "Dwayne The Rock Johnson"}
+	{:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-{:railroads => {}}
+	{:railroads => {}}
 
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	{:railroads => {}}
 
 end
 
