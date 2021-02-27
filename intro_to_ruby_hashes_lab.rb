c51935dbb10d11178973d0a295d7dde2bbd8ade5
Vince W <vincewallgren@me.com>
2021-02-27 05:45:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,12 +12,12 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	{:railroads => {:pieces => 4, :names => {}}}
+	{:railroads => {:pieces => 4}}
 
 end
 
 def monopoly_with_third_tier
-	{:railroads => {:pieces => 4}}
+	{:railroads => {:pieces => 4, :names => {}}}
 
 end
 
