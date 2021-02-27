b3c642e9baca5a0a52578a746c87019f4866c588
Vince W <vincewallgren@me.com>
2021-02-27 06:15:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,7 +52,9 @@ def monopoly_with_fourth_tier
 		:railroads => {
 			:pieces => 4,
 			:names => {
-				:reading_railroad => {},
+				:reading_railroad => {
+					:mortgage_value => "$100"
+				},
 				:pennsylvania_railroad => {},
 				:b_and_o_railroad => {},
 				:shortline => {}
